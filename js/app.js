d3eb772692d65f46b49162130a6d738c81f68faf
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-11 02:08:35 +0600

    user name and comment show

--- js/app.js
+++ js/app.js
@@ -56,6 +56,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const { image, userImage } = post; //solve Profile Picture Issue
+  const commentItem = post.comments;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -128,9 +129,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
